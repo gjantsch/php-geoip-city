5af3ca3cbca52674ad38ffe100fed7e2127bcf4e
gustavo <jantsch@gmail.com>
2019-01-23 19:13:32 -0200

    Fix

--- consultar.php
+++ consultar.php
@@ -42,15 +42,22 @@ if (!empty($ips)) {
 
         echo "<ul>";
         foreach ($resultado as $country => $states) {
-            echo "<li>$country (" . count($states) . ")";
+            echo "<li>$country: " . count($states) . " estados";
             echo "<ul>";
+            $tt_country = 0;
             foreach ($states as $state => $cities) {
-                echo "<li>$state (" . count($cities) . ") <ul>";
+                echo "<li>$state: " . count($cities) . " cidades <ul>";
                 foreach($cities as $city => $ips) {
-                    echo "<li>$city " . count($ips) . "</li>";
+                    echo "<li>$city: " . count($ips) . " ips</li>";
+                    $tt_state += count($ips);
+                    $tt_country += count($ips);
+
                 }
+                echo "<li><strong>Total: $tt_state ips</strong></li>";
                 echo "</ul></li>";
+
             }
+            echo "<li><strong>Total: $tt_country ips</strong></li>";
             echo "</ul></li>";
         }
         echo "</ul>";
@@ -71,5 +78,12 @@ if (!empty($ips)) {
 <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.6/umd/popper.min.js" integrity="sha384-wHAiFfRlMFy6i5SRaxvfOCifBUQy1xHdJ/yoi7FRNXMRBu5WHdZYu1hA6ZOblgut" crossorigin="anonymous"></script>
 <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.2.1/js/bootstrap.min.js" integrity="sha384-B0UglyR+jN6CkvvICOB2joaf5I4l3gm9GU6Hc1og6Ls7i6U/mkkaduKaBhlAXv9k" crossorigin="anonymous"></script>
+<style>
+
+    body {
+        margin: 20px;
+
+    }
+</style>
 </body>
 </html>
